7fe3dd51506d1ade0bf654b4488c064b8c9f88ba
Laptop-Salad <awallis1609@gmail.com>
2024-06-25 23:43:08 +0100

    changed image on home page only for large devices

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,12 @@
 <x-layouts.app>
-    <div class="md:grid md:grid-cols-2 flex flex-col justify-center md:min-h-auto min-h-screen">
-        <div class="md:order-last md:w-full w-1/2 flex justify-center p-2 md:p-20">
-            <img src="images/friend-hug.png" alt="Frendnary logo">
-        </div>
+    <div class="lg:grid lg:grid-cols-2 flex flex-col justify-center lg:min-h-auto min-h-screen">
         <div class="p-6 md:h-screen flex flex-col justify-center items-start space-y-8">
             <h1 class="font-bold text-2xl md:text-8xl">Frendnary</h1>
             <p class="text-lg">Frendnary brings the memories and lore of friend groups into their own central and collaborative private dictionary.</p>
             <x-pack.link :route="route('register')" color="bg-primary">Start here</x-pack.link>
+        </div>
+        <div class="hidden lg:flex md:w-full w-1/2 justify-center p-2 md:p-20">
+            <img src="images/friend-hug.png" alt="Frendnary logo">
         </div>
     </div>
 </x-layouts.app>
